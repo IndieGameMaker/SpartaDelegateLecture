f55f5c29c1e5aa5b4096210e96cbed0b6b7235ad
Lee JaeHyun <myleje@gmail.com>
2025-11-07 16:17:39 +0900

    feat: Implement delegate functionality in PlayerController

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * 델리게이트 (Delegate : 대리자)
+ int a = 1;
+ float b = 2.0f;
+ 
+ function c = function_C; (?)
+ 
+ 델리게이트 변수명 = 메소드(함수)
+ 
+ */
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 1. 델리게이트를 선언
+    private delegate void SampleDelegate();
+    
     void Start()
     {
+        // 델리게이트 변수에 메소드를 할당
+        SampleDelegate sample = Sample01;
         
+        // 델리게이트 실행
+        sample?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Sample01()
     {
-        
+        Debug.Log("Sample01 호출");
     }
 }
